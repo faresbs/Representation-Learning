5b83829ff2e0c3b36b443dda5c8c6d8e6e8badaa
JorgeGtz <jorge.gutierrezor@gmail.com>
2019-02-04 14:26:26 -0500

    script problem 2

---
+++ p2_cnn.py
@@ -0,0 +1,97 @@
+import time
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.init as init
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import torchvision
+import torchvision.transforms
+
+# Define image transformations & Initialize datasets
+mnist_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+mnist_train = torchvision.datasets.MNIST(root='./data', train=True, transform=mnist_transforms, download=True)
+mnist_test = torchvision.datasets.MNIST(root='./data', train=False, transform=mnist_transforms, download=True)
+
+# Create multi-threaded DataLoaders
+trainloader = torch.utils.data.DataLoader(mnist_train, batch_size=64, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(mnist_test, batch_size=64, shuffle=True, num_workers=2)
+
+class Classifier(nn.Module):
+    """CNN Classifier"""
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.conv = nn.Sequential(
+            # Layer 1
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3, 3), padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+
+            # Layer 2
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+
+            # Layer 3
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+
+            # Layer 4
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        )
+        # Logistic Regression
+        self.clf = nn.Linear(128, 10)
+
+    def forward(self, x):
+        return self.clf(self.conv(x).squeeze())
+
+
+cuda_available = torch.cuda.is_available()
+
+clf = Classifier()
+total_parameters = sum([p.data.numpy().size for p in clf.parameters()])
+print('total number of parameters in the network = ' + str(total_parameters))
+
+if cuda_available:
+    clf = clf.cuda()
+optimizer = torch.optim.Adam(clf.parameters(), lr=1e-4)
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(10):
+    losses = []
+    # Train
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if cuda_available:
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+        optimizer.zero_grad()
+        outputs = clf(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+
+    print('Epoch : %d Loss : %.3f ' % (epoch, np.mean(losses)))
+
+    # Evaluate
+    clf.eval()
+    total = 0
+    correct = 0
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        if cuda_available:
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+        outputs = clf(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+    print('Epoch : %d Test Acc : %.3f' % (epoch, 100.*correct/total))
+    print('--------------------------------------------------------------')
+    clf.train()
