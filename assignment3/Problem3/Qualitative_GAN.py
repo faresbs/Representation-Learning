161d3c071dcac9a9e150c98f1cc56ae7491a6e8a
JorgeGtz <jorge.gutierrezor@gmail.com>
2019-04-22 19:22:06 -0400

    adding images for the report

--- assignment3/Problem3/Qualitative_GAN.py
+++ assignment3/Problem3/Qualitative_GAN.py
@@ -73,26 +73,47 @@ def disentangled(z, model, epsilon=3):
 #Accepts one sample z (latent_space)
 #Saves 2+n images (from the two z samples and their (number of alpha) interpolations)
 def interpolating(z0, z1, method, model):
-    alpha = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-    #(a)Interpolate latent space
-    if(method=='latent'):
-        for a in alpha:
-            new_z = a * z0 + (1 - a)*z1
-            sample = model(new_z)
-            save_image(sample.view(1, 3, 32, 32),
-					   'Interpolation/latent space/GANsample_' + str(a) + '.png', normalize=True)
-    #(b)Interpolate image space
-    elif(method=='image'):
-        sample0 = model(z0)
-        save_image(sample0.view(1, 3, 32, 32),
-                   'Interpolation/image space/GANsample0.png', normalize=True)
-        sample1 = model(z1)
-        save_image(sample1.view(1, 3, 32, 32),
-                   'Interpolation/image space/GANsample1.png', normalize=True)
-        for a in alpha:
-            new_sample = a * sample0 + (1 - a) * sample1
-            save_image(new_sample.view(1, 3, 32, 32),
-                       'Interpolation/image space/GANsample_' + str(a) + '.png', normalize=True)
+	alpha = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+
+	sample = torch.zeros([len(alpha),3, 32, 32])
+
+	#(a)Interpolate latent space
+	if(method=='latent'):
+		i = 0
+		for a in alpha:
+			new_z = a * z0 + (1 - a)*z1
+
+			aux = model(new_z)
+			sample[i, :, :, :] = aux.view(1, 3, 32, 32)
+			i += 1
+
+		save_image(sample.view(len(alpha), 3, 32, 32),
+					   'Interpolation/latent space/GANsample.png', normalize=True)
+
+	#(b)Interpolate image space
+
+	elif(method=='image'):
+
+		sample0 = model(z0)
+
+		save_image(sample0.view(1, 3, 32, 32),
+					   'Interpolation/image space/GANsample0.png', normalize=True)
+
+		sample1 = model(z1)
+
+		save_image(sample1.view(1, 3, 32, 32),
+					   'Interpolation/image space/GANsample1.png', normalize=True)
+
+
+		i = 0
+		for a in alpha:
+
+			aux = a * sample0 + (1 - a) * sample1
+			sample[i, :, :, :] = aux.view(1, 3, 32, 32)
+			i += 1
+
+		save_image(sample.view(len(alpha), 3, 32, 32),
+					   'Interpolation/image space/GANsample.png', normalize=True)
 
 
 if __name__ == "__main__":
@@ -130,4 +151,4 @@ if __name__ == "__main__":
     #Sample two z from prior p(z)=N(0,1)
     z1 = torch.randn(1,100).to(device)
     z2 = torch.randn(1,100).to(device)
-    interpolating(z1, z2, 'latent', model)
+    interpolating(z1, z2, 'image', model)
