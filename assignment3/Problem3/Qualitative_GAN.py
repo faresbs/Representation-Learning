d06ea8ca67cdf5269530fa072b9bc84b1f9061ad
JorgeGtz <jorge.gutierrezor@gmail.com>
2019-04-22 18:20:27 -0400

    working on the interpolation

--- assignment3/Problem3/Qualitative_GAN.py
+++ assignment3/Problem3/Qualitative_GAN.py
@@ -69,6 +69,31 @@ def disentangled(z, model, epsilon=3):
     save_image(sample.data[0:25],data_path+'Tests/sample_' + model_name + str(dim1)+'-'+str(dim2) +'_epsilon_' +str(epsilon) +'.png', nrow=5, normalize=True)
     #new_z = z.clone()
 
+#Q3.3
+#Accepts one sample z (latent_space)
+#Saves 2+n images (from the two z samples and their (number of alpha) interpolations)
+def interpolating(z0, z1, method, model):
+    alpha = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+    #(a)Interpolate latent space
+    if(method=='latent'):
+        for a in alpha:
+            new_z = a * z0 + (1 - a)*z1
+            sample = model.decode(new_z)
+            save_image(sample.view(1, 3, 32, 32),
+					   'Interpolation/latent space/sample_' + str(a) + '.png', normalize=True)
+    #(b)Interpolate image space
+    elif(method=='image'):
+        sample0 = model.decode(z0)
+        save_image(sample0.view(1, 3, 32, 32),
+                   'Interpolation/image space/sample0.png', normalize=True)
+        sample1 = model.decode(z1)
+        save_image(sample1.view(1, 3, 32, 32),
+                   'Interpolation/image space/sample1.png', normalize=True)
+        for a in alpha:
+            new_sample = a * sample0 + (1 - a) * sample1
+            save_image(new_sample.view(1, 3, 32, 32),
+                       'Interpolation/image space/sample_' + str(a) + '.png', normalize=True)
+
 
 if __name__ == "__main__":
     ###Qualitative Evaluation
